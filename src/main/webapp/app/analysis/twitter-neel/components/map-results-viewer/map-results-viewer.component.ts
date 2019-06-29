812f65b35df42745a1ab9420238e70d6d38790db
Fausto <fausto91@gmail.com>
2019-06-29 22:11:48 +0200

    Implementazione componente MapResultsViewer

--- src/main/webapp/app/analysis/twitter-neel/components/map-results-viewer/map-results-viewer.component.ts
+++ src/main/webapp/app/analysis/twitter-neel/components/map-results-viewer/map-results-viewer.component.ts
@@ -1,39 +1,168 @@
-import {Component, OnInit} from '@angular/core';
-import {ResultsViewerComponent} from 'app/analysis/twitter-neel/components/results-viewer.component';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ResultsViewerComponent } from 'app/analysis/twitter-neel/components/results-viewer.component';
+import { ILocation, LocationSource } from 'app/analysis/twitter-neel/models/location.model';
+import { select, Store } from '@ngrx/store';
+import {
+    buildNilEntityIdentifier,
+    selectAllResources,
+    selectAllTweets, selectLocationsBySource,
+    selectNilEntities, selectNilEntitiesTweetsCount,
+    selectResourcesTweetsCount,
+    TwitterNeelState
+} from 'app/analysis/twitter-neel';
+import { Observable } from 'rxjs';
+import { IAnalysis } from 'app/analysis';
+import { AnalysisState, selectCurrentAnalysis } from 'app/analysis/store';
+import { ILinkedEntity, INeelProcessedTweet, INilEntity, IResource } from 'app/analysis/twitter-neel/models/neel-processed-tweet.model';
+import { map, throttleTime } from 'rxjs/operators';
 
 @Component({
     templateUrl: './map-results-viewer.component.html',
-    styleUrls: ['./map-results-viewer.component.scss']
+    styleUrls: ['./map-results-viewer.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MapResultsViewerComponent extends ResultsViewerComponent implements OnInit {
 
-    public lat = 10;
-    public lng = 10;
-    public zoom = 3;
-    public markers: Marker[] = [];
+    currentAnalysis$: Observable<IAnalysis>;
+    tweets$: Observable<INeelProcessedTweet[]>;
+    // statuses$: Observable<ITwitterStatus[]>;
+    nilEntities$: Observable<INilEntity[]>;
+    linkedEntities$: Observable<ILinkedEntity[]>;
+    resources$: Observable<IResource[]>;
+    // locations$: Observable<ILocation[]>;
+    statusLocations$: Observable<ILocation[]>;
+    userLocations$: Observable<ILocation[]>;
+    resourceLocations$: Observable<ILocation[]>;
 
-    mapClicked(e) {}
-    clickedMarker(m, i) {}
-    markerDragEnd(m, e) {}
+    resourcesCounter: {[key: string]: number} = {};
+    nilEntitiesCounter: {[key: string]: number} = {};
+
+    selectedTweet: INeelProcessedTweet = null;
+    selectedResource: IResource = null;
+    selectedNilEntity: INilEntity = null;
+
+    filteredTweets$: Observable<INeelProcessedTweet[]>;
+    paginatedTweets$: Observable<INeelProcessedTweet[]>;
+
+    currentPage = 1;
+    pageSize = 250;
+
+    constructor(private changeDetector: ChangeDetectorRef,
+                private analysisStore:  Store<AnalysisState>,
+                private tNeelStore: Store<TwitterNeelState>) {
+        super();
+    }
 
     ngOnInit(): void {
-        for (let i = 0; i < 1000; ++i) {
-            this.markers.push(
-                {
-                    label: 'prova' + i,
-                    lat: 10 + (Math.random()),
-                    lng: 10 + (Math.random()),
-                    draggable: false
-                }
-            );
+        this.currentAnalysis$ = this.analysisStore.pipe(select(selectCurrentAnalysis));
+
+        this.tweets$ = this.tNeelStore.pipe(select(selectAllTweets));
+        this.nilEntities$ = this.tNeelStore.pipe(select(selectNilEntities));
+        this.resources$ = this.tNeelStore.pipe(select(selectAllResources));
+        this.paginatedTweets$ = this.tweets$.pipe(
+            map(tweets => tweets.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize))
+        );
+
+        this.tNeelStore.pipe(select(selectResourcesTweetsCount)).subscribe(counters => {
+            this.resourcesCounter = counters;
+        });
+
+        this.tNeelStore.pipe(select(selectNilEntitiesTweetsCount)).subscribe(counters => {
+            this.nilEntitiesCounter = counters;
+        });
+
+        this.statusLocations$ = this.tNeelStore.pipe(select(selectLocationsBySource(LocationSource.Status)));
+        this.resourceLocations$ = this.tNeelStore.pipe(select(selectLocationsBySource(LocationSource.Resource)));
+        this.userLocations$ = this.tNeelStore.pipe(select(selectLocationsBySource(LocationSource.TwitterUser)));
+    }
+
+    stringifyTweetEntities(entities: ILinkedEntity[]) {
+        return entities.map(e => e.value).join(', ');
+    }
+
+    nilEntityTweetsCount(entity: INilEntity) {
+        return this.nilEntitiesCounter[buildNilEntityIdentifier(entity)];
+    }
+
+    onTweetClick(tweet: INeelProcessedTweet) {
+        if (this.selectedTweet !== null && this.selectedTweet.id === tweet.id) {
+            this.selectedTweet = null;
+        } else {
+            this.selectedTweet = tweet;
+            this.selectedNilEntity = null;
+            this.selectedResource = null;
         }
     }
 
-}
+    onResourceClick(resource: IResource) {
+        if (this.selectedResource !== null && this.selectedResource.url === resource.url) {
+            this.selectedResource = null;
+        } else {
+            this.selectedTweet = null;
+            this.selectedNilEntity = null;
+            this.selectedResource = resource;
 
-interface Marker {
-    label: String;
-    lat: number;
-    lng: number;
-    draggable: boolean;
+            this.filteredTweets$ = this.tweets$
+                .pipe(
+                    throttleTime(5000),
+                    map(allTweets => allTweets.filter(t => t.entities && t.entities
+                        .some(e => e.resource && e.resource.url === resource.url))),
+                    map(tweets => tweets.slice(0, this.pageSize)),
+                );
+        }
+    }
+
+    onNilEntityClick(entity: INilEntity) {
+        if (this.selectedNilEntity !== null &&
+            this.selectedNilEntity.value === entity.value &&
+            this.selectedNilEntity.nilCluster === entity.nilCluster) {
+            this.selectedNilEntity = null;
+        } else {
+            this.selectedTweet = null;
+            this.selectedNilEntity = entity;
+            this.selectedResource = null;
+
+            this.filteredTweets$ = this.tweets$
+                .pipe(
+                    throttleTime(5000),
+                    map(allTweets => allTweets.filter(t => t.entities && t.entities
+                        .some(e => e.isNil && e.value === entity.value && e.nilCluster === entity.nilCluster))),
+                    map(tweets => tweets.slice(0, this.pageSize)),
+                );
+        }
+    }
+
+    tweetCssClass(tweet: INeelProcessedTweet) {
+        if (this.selectedTweet != null) {
+            return (this.selectedTweet.id === tweet.id) ? 'status active' : 'status inactive';
+        } else {
+            return '';
+        }
+    }
+
+    resourceCssClass(resource: IResource) {
+        if (this.selectedResource != null) {
+            return (this.selectedResource.url === resource.url) ? 'resource active' : 'resource inactive';
+        } else {
+            return '';
+        }
+    }
+
+    nilEntityCssClass(entity: INilEntity) {
+        if (this.selectedNilEntity != null) {
+            return (this.selectedNilEntity.value === entity.value && this.selectedNilEntity.nilCluster === entity.nilCluster) ?
+                'resource active' : 'resource inactive';
+        } else {
+            return '';
+        }
+    }
+
+    selectedTweetLinkedEntities() {
+        return this.selectedTweet.entities.filter(e => !e.isNil);
+    }
+
+    selectedTweetNilEntities() {
+        return this.selectedTweet.entities.filter(e => e.isNil);
+    }
+
 }
--- src/main/webapp/app/analysis/twitter-neel/components/query-view/query-view.component.ts
+++ src/main/webapp/app/analysis/twitter-neel/components/query-view/query-view.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { ActionTypes, AnalysisState, GetAnalysis, selectCurrentAnalysis, selectLastError } from 'app/analysis/store';
+import { Observable } from 'rxjs';
+import { IAnalysis, AnalysisStatus } from 'app/analysis';
+import { first, take } from 'rxjs/operators';
+import { StartListenTwitterNeelResults, StopListenTwitterNeelResults } from 'app/analysis/twitter-neel';
 
 @Component({
   selector: 'btw-query-view',
   templateUrl: './query-view.component.html',
   styleUrls: ['./query-view.component.scss']
 })
-export class QueryViewComponent implements OnInit {
+export class QueryViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    currentAnalysis$: Observable<IAnalysis>;
+    lastError$: Observable<any>;
 
-  ngOnInit() {
-  }
+    get currentAnalysis(): IAnalysis {
+        let currentAnalysis: IAnalysis = null;
+        this.currentAnalysis$
+            .pipe(take(1))
+            .subscribe((analysis: IAnalysis) => currentAnalysis = analysis);
 
+        return currentAnalysis;
+    }
+
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private analysisStore: Store<AnalysisState>
+    ) { }
+
+    ngOnInit() {
+        this.currentAnalysis$ = this.analysisStore.select(selectCurrentAnalysis);
+        this.lastError$ = this.analysisStore.pipe(select(selectLastError));
+
+        this.currentAnalysis$.subscribe((analysis: IAnalysis) => {
+            this.onCurrentAnalysisChange(analysis);
+        });
+
+        this.lastError$.pipe(first(e => e && e.type === ActionTypes.GetAnalysisError)).subscribe(e => {
+            this.router
+                .navigate(['/analysis/twitter-neel/query/new'])
+                .catch(err => console.error(err));
+        });
+
+        this.route.paramMap.subscribe(params => {
+            this.onRouteAnalysisIdChange(params.get('analysisId'));
+        });
+
+        this.onRouteAnalysisIdChange(this.route.snapshot.params.analysisId);
+    }
+
+    ngOnDestroy(): void {
+        console.log('qui');
+    }
+
+    onRouteAnalysisIdChange(analysisId: string) {
+        if (analysisId && (!this.currentAnalysis || this.currentAnalysis.id !== analysisId)) {
+            this.analysisStore.dispatch(new GetAnalysis(analysisId));
+        }
+    }
+
+    onCurrentAnalysisChange(analysis: IAnalysis) {
+        if (analysis && analysis.status === AnalysisStatus.Running) {
+            this.startListenResults(analysis);
+        } else {
+            this.stopListenResults(analysis);
+        }
+    }
+
+    startListenResults(analysis: IAnalysis) {
+        this.analysisStore.dispatch(new StartListenTwitterNeelResults(analysis.id));
+    }
+
+    stopListenResults(analysis?: IAnalysis) {
+        const analysisId = analysis ? analysis.id : null;
+        this.analysisStore.dispatch(new StopListenTwitterNeelResults(analysisId));
+    }
 }
