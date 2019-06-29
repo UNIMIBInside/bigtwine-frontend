608f83a5b6fd6044ec45152eeef77bf3a7d4aa47
Fausto <fausto91@gmail.com>
2019-06-29 22:05:01 +0200

    Implementazione component query toolbar

--- src/main/webapp/app/analysis/twitter-neel/components/query-toolbar/query-toolbar.component.ts
+++ src/main/webapp/app/analysis/twitter-neel/components/query-toolbar/query-toolbar.component.ts
@@ -1,20 +1,155 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router, RouterEvent } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { AnalysisState, CreateAnalysis, selectCurrentAnalysis, StartAnalysis, StartListenAnalysisChanges, StopAnalysis, StopListenAnalysisChanges } from 'app/analysis/store';
+import { Observable, Subscription } from 'rxjs';
+import { AnalysisInputType, AnalysisStatus, AnalysisType, IAnalysis } from 'app/analysis';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
-  selector: 'btw-query-toolbar',
-  templateUrl: './query-toolbar.component.html',
-  styleUrls: ['./query-toolbar.component.scss']
+    selector: 'btw-query-toolbar',
+    templateUrl: './query-toolbar.component.html',
+    styleUrls: ['./query-toolbar.component.scss']
 })
-export class QueryToolbarComponent implements OnInit {
+export class QueryToolbarComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+    @Input() mode = 'new';
+    @Output() queryChange = new EventEmitter();
+    private query: string;
+    private currentViewMode: string;
 
-  ngOnInit() {
-  }
+    currentAnalysis$: Observable<IAnalysis>;
+    subscriptions = new Subscription();
 
-  switchViewMode(mode) {
-    this.router.navigate([{'outlets': {'results-viewer': mode}}], {relativeTo: this.route});
-  }
+    get currentAnalysis(): IAnalysis {
+        let currentAnalysis: IAnalysis = null;
+        this.currentAnalysis$
+            .pipe(take(1))
+            .subscribe((analysis: IAnalysis) => currentAnalysis = analysis);
 
+        return currentAnalysis;
+    }
+
+    get showCreateButton() {
+        return this.mode === 'new' || (this.currentAnalysis && this.currentAnalysis.query !== this.query);
+    }
+
+    get showStartStopButton() {
+        return !this.showCreateButton;
+    }
+
+    get showResultsViewerButton() {
+        return this.mode === 'view';
+    }
+
+    constructor(private router: Router, private route: ActivatedRoute, private analysisStore: Store<AnalysisState>) {
+    }
+
+    ngOnInit() {
+        this.currentAnalysis$ = this.analysisStore.pipe(select(selectCurrentAnalysis));
+
+        const s1 = this.currentAnalysis$.subscribe((analysis: IAnalysis) => {
+            this.onCurrentAnalysisChange(analysis);
+        });
+
+        const s2 = this.router.events.pipe(filter(e => e instanceof RouterEvent)).subscribe((e: RouterEvent) => {
+            this.onRouteChange(e);
+        });
+
+        this.onRouteChange(null);
+
+        this.subscriptions.add(s1);
+        this.subscriptions.add(s2);
+
+        if (this.currentAnalysis && this.mode === 'view') {
+            this.query = this.currentAnalysis.query;
+        }
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.unsubscribe();
+    }
+
+    onQueryChange() {
+        this.queryChange.emit(this.query);
+    }
+
+    onCreateBtnClick() {
+        this.createAnalysis(this.query);
+    }
+
+    onToggleAnalysisBtnClick() {
+        if (this.currentAnalysis) {
+            if (this.currentAnalysis.status === AnalysisStatus.Ready || this.currentAnalysis.status === AnalysisStatus.Stopped) {
+                this.startAnalysis(this.currentAnalysis);
+            } else if (this.currentAnalysis.status === AnalysisStatus.Running) {
+                this.stopAnalysis(this.currentAnalysis);
+            }
+        }
+    }
+
+    onToggleViewModeBtnClick() {
+        const currentMode = this.currentViewMode;
+        const isMapView = currentMode === 'map';
+
+        this.switchViewMode(isMapView ? 'list' : 'map');
+    }
+
+    onCurrentAnalysisChange(analysis: IAnalysis) {
+        if (this.isSupportedAnalysis(analysis)) {
+            this.router
+                .navigate(['/analysis/twitter-neel/query/view/' + analysis.id])
+                .catch(e => console.error(e));
+        }
+    }
+
+    onRouteChange(event: RouterEvent) {
+        this.currentViewMode = this.getCurrentViewMode();
+    }
+
+    isSupportedAnalysis(analysis: IAnalysis) {
+        return (analysis && analysis.id && analysis.type === 'twitter-neel' && analysis.inputType === 'query');
+    }
+
+    createAnalysis(query: string) {
+        const analysis: IAnalysis = {
+            type: AnalysisType.TwitterNeel,
+            inputType: AnalysisInputType.Query,
+            query,
+        };
+
+        this.analysisStore.dispatch(new CreateAnalysis(analysis));
+    }
+
+    startAnalysis(analysis: IAnalysis) {
+        this.analysisStore.dispatch(new StartAnalysis(analysis.id));
+    }
+
+    stopAnalysis(analysis: IAnalysis) {
+        this.analysisStore.dispatch(new StopAnalysis(analysis.id));
+    }
+
+    startListenAnalysisChanges(analysis: IAnalysis) {
+        this.analysisStore.dispatch(new StartListenAnalysisChanges(analysis.id));
+    }
+
+    stopListenAnalysisChanges(analysis?: IAnalysis) {
+        const analysisId = analysis ? analysis.id : null;
+        this.analysisStore.dispatch(new StopListenAnalysisChanges(analysisId));
+    }
+
+    getCurrentViewMode(): string {
+        const path = this.route.snapshot.children
+            .filter(r => r.outlet === 'results-viewer')
+            .map(r => r.routeConfig.path)
+            .shift();
+
+        return path === '' ? 'map' : path;
+    }
+
+    switchViewMode(mode: string) {
+        this.router
+            .navigate([{'outlets': {'results-viewer': mode}}], {relativeTo: this.route})
+            .catch(e => console.error(e));
+    }
 }
---
+++ src/test/javascript/spec/app/analysis/twitter-neel/components/query-toolbar.component.spec.ts
@@ -0,0 +1,105 @@
+import { BigtwineTestModule } from '../../../../test.module';
+import { ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
+import { StoreModule, Store } from '@ngrx/store';
+import { QueryToolbarComponent } from 'app/analysis/twitter-neel/components';
+import * as fromAnalysis from 'app/analysis/store';
+import * as fromTwitterNeel from 'app/analysis/twitter-neel/store';
+import { BigtwineSharedLibsModule } from 'app/shared';
+import { of } from 'rxjs';
+import { AnalysisService, IAnalysis } from 'app/analysis';
+import { EffectsModule } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+describe('QueryToolbar Component', () => {
+    let component: QueryToolbarComponent;
+    let fixture: ComponentFixture<QueryToolbarComponent>;
+    let store: Store<fromTwitterNeel.TwitterNeelState>;
+    let router: Router;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BigtwineTestModule,
+                BigtwineSharedLibsModule,
+                StoreModule.forRoot({}),
+                StoreModule.forFeature('analysis', fromAnalysis.AnalysisReducer),
+                StoreModule.forFeature('twitterNeel', fromTwitterNeel.TwitterNeelReducer),
+                EffectsModule.forRoot([]),
+                EffectsModule.forFeature([fromAnalysis.AnalysisEffects, fromTwitterNeel.TwitterNeelEffects]),
+            ],
+            declarations: [
+                QueryToolbarComponent,
+            ],
+            providers: [],
+        });
+
+        router = TestBed.get(Router);
+        store = TestBed.get(Store);
+
+        spyOn(store, 'dispatch').and.callThrough();
+        // spyOn(router, 'navigate').and.callThrough();
+
+        fixture = TestBed.createComponent(QueryToolbarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should dispatch an action to create analysis', (() => {
+        const query = 'test';
+        const action = new fromAnalysis.CreateAnalysis({
+            type: 'twitter-neel',
+            inputType: 'query',
+            query,
+        });
+
+        component.createAnalysis(query);
+
+        expect(store.dispatch).toHaveBeenCalledWith(action);
+    }));
+
+    it('should update current analysis', inject(
+        [AnalysisService],
+        fakeAsync((service: AnalysisService) => {
+            const query = 'test';
+            spyOn(service, 'createAnalysis').and.returnValue(of({
+                id: 'testanalysis1',
+                type: 'twitter-neel',
+                inputType: 'query',
+                query,
+            }));
+
+            component.createAnalysis(query);
+
+            component.currentAnalysis$.subscribe((currentAnalysis: IAnalysis) => {
+                expect(currentAnalysis).not.toBeNull();
+                expect(currentAnalysis.id).toBe('testanalysis1');
+            });
+
+            expect(router.navigate).toHaveBeenCalledTimes(1);
+        })
+    ));
+
+    it('should submit on button click', () => {
+        const query = 'test';
+        const action = new fromAnalysis.CreateAnalysis({
+            type: 'twitter-neel',
+            inputType: 'query',
+            query,
+        });
+
+        const input = fixture.debugElement.query(By.css('input[name="query"]')).nativeElement as HTMLInputElement;
+        const button = fixture.debugElement.query(By.css('button[name="submit"]')).nativeElement as HTMLButtonElement;
+
+        input.value = query;
+        input.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+        button.dispatchEvent(new Event('click'));
+
+        expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
+});
