df754187ed280ebff6f545b2c7600fee91809501
Fausto <fausto91@gmail.com>
2019-10-14 14:28:15 +0200

    Configurazione per produzione

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -63,7 +63,7 @@ module.exports = (options) => ({
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-                SERVER_API_URL: `'http://localhost:9000/'`
+                SERVER_API_URL: options.env === 'development' ? `'http://localhost:9000/'` : `'http://api.bigtwine.doublerew.net/'`
             }
         }),
         new CopyWebpackPlugin([
