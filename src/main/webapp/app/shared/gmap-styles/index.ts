27e0bcb6b8845e7b7fa9e2c0daac58f220095903
Fausto <fausto91@gmail.com>
2019-10-19 20:19:01 +0200

    Aggiornato stile predefinito mappa

--- src/main/webapp/app/shared/gmap-styles/index.ts
+++ src/main/webapp/app/shared/gmap-styles/index.ts
@@ -1,4 +1,5 @@
 import NEUTRAL_BLUE from './neutral-blue';
+import NEUTRAL_GRAY from './neutral-gray';
 
-export { NEUTRAL_BLUE };
-export default NEUTRAL_BLUE;
+export { NEUTRAL_BLUE, NEUTRAL_GRAY };
+export default NEUTRAL_GRAY;
---
+++ src/main/webapp/app/shared/gmap-styles/neutral-gray.ts
@@ -0,0 +1,104 @@
+/* tslint:disable */
+export const MAP_STYLE = [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#29303B"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#616975"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#1A2533"
+            },
+            {
+                "weight": 2
+            },
+            {
+                "gamma": 0.84
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": 0.6
+            },
+            {
+                "color": "#1A2533"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+];
+
+export default MAP_STYLE;
